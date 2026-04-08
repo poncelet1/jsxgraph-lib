35fdc3e5a981f954aafc203243c71d2c47e9cd6b
poncelet1 <nsato7@yahoo.ca>
2026-04-08 16:33:51 -0700

    Update functions.js

--- functions.js
+++ functions.js
@@ -24,14 +24,16 @@ const jg = {
     pointStyle: {
         size: 2,             // Default is usually 3 or 4; 2 is nice and small
         strokeColor: 'black', // The outline of the point
-        fillColor: 'black'   // The inside of the point
+        fillColor: 'black',   // The inside of the point
+        highlight: false
     },
 
     pointStyleNoLabel: {
         size: 2,             // Default is usually 3 or 4; 2 is nice and small
         strokeColor: 'black', // The outline of the point
         fillColor: 'black',   // The inside of the point
-        withLabel: false
+        withLabel: false,
+        highlight: false
     },
 
     lineStyle: {
@@ -84,6 +86,40 @@ const jg = {
         return (B.Dist(C) + C.Dist(A) + A.Dist(B))/2;
     },
 
+    /**
+     * Given points A, B, C, returns the area of triangle ABC
+     * @param A first point
+     * @param B second point
+     * @param C third point
+     * @returns area
+     */
+    area: function(A, B, C) {
+    const ax = (typeof A.X === 'function') ? A.X() : A[0];
+    const ay = (typeof A.Y === 'function') ? A.Y() : A[1];
+    const bx = (typeof B.X === 'function') ? B.X() : B[0];
+    const by = (typeof B.Y === 'function') ? B.Y() : B[1];
+    const cx = (typeof C.X === 'function') ? C.X() : C[0];
+    const cy = (typeof C.Y === 'function') ? C.Y() : C[1];
+        return Math.abs(ax * by + bx * cy + cx * ay - ay * bx - by * cx - cy * ax)/2;
+    },
+
+    /**
+     * Given points A, B, C, returns the signed area of triangle ABC
+     * @param A first point
+     * @param B second point
+     * @param C third point
+     * @returns signed area
+     */
+    signedArea: function(A, B, C) {
+    const ax = (typeof A.X === 'function') ? A.X() : A[0];
+    const ay = (typeof A.Y === 'function') ? A.Y() : A[1];
+    const bx = (typeof B.X === 'function') ? B.X() : B[0];
+    const by = (typeof B.Y === 'function') ? B.Y() : B[1];
+    const cx = (typeof C.X === 'function') ? C.X() : C[0];
+    const cy = (typeof C.Y === 'function') ? C.Y() : C[1];
+        return (ax * by + bx * cy + cx * ay - ay * bx - by * cx - cy * ax)/2;
+    },
+
    /**
      * Given points A, B, C, returns the centroid of triangle ABC
      * @param A first point
@@ -123,6 +159,21 @@ const jg = {
     return [(ax * x + bx * y + cx * z)/(x + y + z), (ay * x + by * y + cy * z)/(x + y + z)];
 },
 
+    /**
+     * Given points A, B, C, returns the A-excenter of triangle ABC
+     * @param A first point
+     * @param B second point
+     * @param C third point
+     * @returns A-excenter
+     */
+   exradius: function(A, B, C) {
+       const a = B.Dist(C);
+       const s = this.semiperimeter(A, B, C);
+       const K = this.area(A, B, C);
+
+       return K/(s - a);
+   }
+       
     /**
      * Given points A, B, C, returns the A-excenter of triangle ABC
      * @param A first point
