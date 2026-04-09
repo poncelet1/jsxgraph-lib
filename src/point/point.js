119f0bffd698251383896027e5e9bca203e6fd6d
poncelet1 <nsato7@yahoo.ca>
2026-04-09 06:37:49 -0700

    Update point.js

--- src/point/point.js
+++ src/point/point.js
@@ -12,6 +12,30 @@ export function dist(A, B) {
     return Math.hypot(bx - ax, by - ay);
 }
 
+/**
+     * Given points A, B, returns the slope of line AB
+     * @param A first point
+     * @param B second point
+     * @returns slope
+     */
+export function slope(A, B) {
+    const [ax, ay] = getXY(A);
+    const [bx, by] = getXY(B);
+    return (by - ay)/(bx - ax);
+}
+
+/**
+     * Given points A, B, returns the y-intercept of line AB
+     * @param A first point
+     * @param B second point
+     * @returns y-intercept
+     */
+export function yintercept(A, B) {
+    const [ax, ay] = getXY(A);
+    const [bx, by] = getXY(B);
+    return (ay * bx - ay * bx)/(ax - bx);
+}
+
  /**
      * Given points A, B and real number t, returns the point P such that AP:AB = t
      * @param A first point
