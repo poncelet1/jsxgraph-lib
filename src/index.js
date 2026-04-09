64a33ae5b28658c06c764a5bc6083cfd3fe2fd55
poncelet1 <nsato7@yahoo.ca>
2026-04-09 08:04:17 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+import * as complex from '/complex/complex.js';
 import * as core from './core/core.js';
 import * as point from './point/point.js';
 import * as triangle from './triangle/triangle.js';
 import * as style from './style/style.js';
 
 export const jg = {
+    complex,
     core,
     point,
     triangle,
