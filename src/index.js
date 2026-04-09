fc90cbf82aa30f13af2ee7e120189a142c2619d2
poncelet1 <nsato7@yahoo.ca>
2026-04-08 21:04:37 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import * as core from './core/core.js';
 import * as point from './point/point.js';
 import * as triangle from './triangle/triangle.js';
-import * as style from './style/styles.js';
+import * as style from './style/style.js';
 
 export const jg = {
     core,
