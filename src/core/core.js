f9b045c97a8ba69991d9ffdf09844f80266df773
poncelet1 <nsato7@yahoo.ca>
2026-04-08 20:53:02 -0700

    Update core.js

--- src/core/core.js
+++ src/core/core.js
@@ -1,3 +1,8 @@
+ /**
+     * Given point P, returns x and y coordinates
+     * @param P point
+     * @returns [x, y]
+     */
 export function getXY(P) {
     return (typeof P.X === 'function')
         ? [P.X(), P.Y()]
