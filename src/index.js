86ed08f18af7a5d874965fe34aa1b223166fe9b2
poncelet1 <nsato7@yahoo.ca>
2026-04-08 20:51:45 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-import * as core from './core/pointUtils.js';
-import * as point from './point/pointOps.js';
+import * as core from './core/core.js';
+import * as point from './point/point.js';
 import * as triangle from './triangle/triangle.js';
-import * as jsxgraph from './jsxgraph/circles.js';
 import * as style from './style/styles.js';
 
 export const jg = {
     core,
     point,
     triangle,
-    jsxgraph,
     style
 };
 
