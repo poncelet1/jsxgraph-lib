a41bf14cf157a2b74c749577328d5087e87364db
poncelet1 <nsato7@yahoo.ca>
2026-04-08 20:50:41 -0700

    Create style.js

---
+++ src/style/style.js
@@ -0,0 +1,46 @@
+export const boardStyle = {
+    boundingbox: [-5, 5, 5, -5],
+    axis: true,
+    zoom: { wheel: true, needShift: false },
+    pan: { enabled: true, needShift: false, needCtrl: false }
+};
+
+export const pointStyle = {
+    size: 2,
+    strokeColor: 'black',
+    fillColor: 'black',
+    highlight: false,
+    showInfobox: false
+};
+
+export const pointStyleNoLabel = {
+    size: 2,
+    strokeColor: 'black',
+    fillColor: 'black',
+    withLabel: false,
+    highlight: false
+};
+
+export const lineStyle = {
+    strokeWidth: 1,
+    fixed: true,
+    highlight: false
+};
+
+export const triangleStyle = {
+    fixed: true,
+    draggable: false,
+    highlight: false,
+    borders: {
+        fixed: true,
+        draggable: false,
+        highlight: false
+    }
+};
+
+export const circleStyle = {
+    strokeWidth: 1,
+    fixed: true,
+    draggable: false,
+    highlight: false
+};
