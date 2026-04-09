3fa27640ed41f4ad28f38c88484269ba93940dc6
poncelet1 <nsato7@yahoo.ca>
2026-04-08 20:53:35 -0700

    Update point.js

--- src/point/point.js
+++ src/point/point.js
@@ -1,11 +1,24 @@
-import { getXY } from '../core/pointUtils.js';
+import { getXY } from '../core/core.js';
 
+/**
+     * Given points A, B, returns the distance betweeen A and B
+     * @param A first point
+     * @param B second point
+     * @returns distance
+     */
 export function dist(A, B) {
     const [ax, ay] = getXY(A);
     const [bx, by] = getXY(B);
     return Math.hypot(bx - ax, by - ay);
 }
 
+ /**
+     * Given points A, B and real number t, returns the point P such that AP:AB = t
+     * @param A first point
+     * @param B second point
+     * @param t real number
+     * @returns interpolated point
+     */
 export function interp(A, B, t) {
     const [ax, ay] = getXY(A);
     const [bx, by] = getXY(B);
