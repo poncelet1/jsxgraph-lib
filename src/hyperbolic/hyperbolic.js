8b2efcc7e4b97116f82280362728a8d5a828dfc9
poncelet1 <nsato7@yahoo.ca>
2026-04-10 09:28:39 -0700

    Update hyperbolic.js

--- src/hyperbolic/hyperbolic.js
+++ src/hyperbolic/hyperbolic.js
@@ -143,29 +143,3 @@ export function hyperline(board, A, B, style = circleStyle) {
     
     return board.create('arc', [arcCenter, startPoint, endPoint], style);
 }
-
-export function hyperline2(board, A, B, style = circleStyle) {
-    const C = () => clineCenter(A, B);
-    const P = () => hyperlineEndPoint(A, B);
-    const Q = () => hyperlineEndPoint(B, A);
-
-    const orientation = () => signedArea(C(), A, B);
-
-    return board.create('arc', [
-                C,
-        () => orientation() > 0 ? P() : Q(),
-        () => orientation() > 0 ? Q() : P()
-    ], {
-        ...style,
-
-        // REQUIRED to suppress implicit point creation
-        point: {
-            visible: false,
-            withLabel: false,
-            fixed: true
-        },
-
-        // Some JSXGraph builds require this too
-        withLines: false
-    });
-}
