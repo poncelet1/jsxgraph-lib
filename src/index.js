a8d467b941bb8410b1f111d7c6db9ad56b40fb45
poncelet1 <nsato7@yahoo.ca>
2026-04-09 18:54:31 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import * as complex from './complex/complex.js';
 import * as core from './core/core.js';
-//import * as hyperbolic from './hyperbolic/hyperbolic.js';
+import * as hyperbolic from './hyperbolic/hyperbolic.js';
 import * as point from './point/point.js';
 import * as triangle from './triangle/triangle.js';
 import * as style from './style/style.js';
@@ -8,7 +8,7 @@ import * as style from './style/style.js';
 export const jg = {
     complex,
     core,
-    //hyperbolic,
+    hyperbolic,
     point,
     style,
     triangle
