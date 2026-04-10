301a04e252fb723b2429fc0397fb693b12357096
poncelet1 <nsato7@yahoo.ca>
2026-04-09 19:45:47 -0700

    Update hyperbolic.js

--- src/hyperbolic/hyperbolic.js
+++ src/hyperbolic/hyperbolic.js
@@ -2,7 +2,7 @@ import { getXY } from '../core/core.js';
 import { complexConjugate, complexDifference, complexProduct, complexQuotient, complexSum } from '../complex/complex.js';
 
 /**
-     * Given point Z, A, returns (Z - A)/(1 - conj(A)*Z)
+     * Given points Z, A, returns (Z - A)/(1 - conj(A)*Z)
      * @param Z first point
      * @param A second point
      * @returns (Z - A)/(1 - conj(A)*Z)
@@ -12,4 +12,39 @@ export function movePointToOrigin(Z, A) {
     const [zx, zy] = getXY(Z);
     
     return complexQuotient(complexDifference(Z, A), complexDifference([1, 0], complexProduct(complexConjugate(Z), A)));
+}
+
+/**
+     * Given points Z, A, returns (Z + A)/(1 + conj(A)*Z)
+     * @param Z first point
+     * @param A second point
+     * @returns (Z + A)/(1 + conj(A)*Z)
+     */
+export function movePointFromOrigin(Z, A) {
+    const [ax, ay] = getXY(A);
+    const [zx, zy] = getXY(Z);
+    
+    return complexQuotient(complexSum(Z, A), complexSum([1, 0], complexProduct(complexConjugate(Z), A)));
+}
+
+/**
+     * Given points A, B, returns hyperbolic distance between A and B
+     * @param A first point
+     * @param B second point
+     * @returns hyperbolic distance
+     */
+export function hyperDist(A, B) {
+    const Bp = movePointToOrigin(B, A);
+    return Math.log((1 + Bp.L())/(1 - Bp.L()));
+}
+
+/**
+     * Given points A, B, returns hyperbolic distance between A and B
+     * @param A first point
+     * @param B second point
+     * @returns hyperbolic distance
+     */
+export function hyperDist(A, B) {
+    const Bp = movePointToOrigin(B, A);
+    return Math.log((1 + Bp.L())/(1 - Bp.L()));
 }
