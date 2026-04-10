23eb549abdda41ce4e5c35ebd0e9ca1513d85fdf
poncelet1 <nsato7@yahoo.ca>
2026-04-09 19:35:33 -0700

    Update hyperbolic.js

--- src/hyperbolic/hyperbolic.js
+++ src/hyperbolic/hyperbolic.js
@@ -11,5 +11,5 @@ export function movePointToOrigin(Z, A) {
     const [ax, ay] = getXY(A);
     const [zx, zy] = getXY(Z);
     
-    return complexQuotient(complexDifference(Z, A), complexDifference([1, 0], complexProduct(complexQuotient(Z), A)));
+    return complexQuotient(complexDifference(Z, A), complexDifference([1, 0], complexProduct(complexConjugate(Z), A)));
 }
