4eb3340670360f54a262fba454406ab5fff1f40c
poncelet1 <nsato7@yahoo.ca>
2026-04-12 10:28:17 -0700

    Update conic.js

--- src/conic/conic.js
+++ src/conic/conic.js
@@ -39,7 +39,24 @@ export function conicxy(A, B, C, D, E) {
     return signedArea(B, C, E) * signedArea(D, A, E) * ((ay - by) * (dx - cx) + (bx - ax) * (cy - dy)) - signedArea(A, B, E) * signedArea (C, D, E) * ((by - cy) * (ax - dx) + (cx - bx) * (dy - ay));
 }
 
-
+/**
+     * Given points A, B, C, D, E, returns the coefficient of y^2 in the equation of the conic passing through the five points
+     * @param A first point
+     * @param B second point
+     * @param C third point
+     * @param D fourth point
+     * @param E fifth point
+     * @returns coefficient of y^2
+     */
+export function conicyy(A, B, C, D, E) {
+    const [ax, ay] = getXY(A);
+    const [bx, by] = getXY(B);
+    const [cx, cy] = getXY(C);
+    const [dx, dy] = getXY(D);
+    const [ex, ey] = getXY(E);
+  
+    return signedArea(B, C, E) * signedArea(D, A, E) * (bx - ax) * (dx - cx) - signedArea(A, B, E) * signedArea (C, D, E) * (cx - bx) * (ax - dx);
+}
 
 
 
