525c869cd7a380002818e9cdaa95ca0d22157a1a
poncelet1 <nsato7@yahoo.ca>
2026-04-12 08:44:27 -0700

    Update inversion.js

--- src/inversion/inversion.js
+++ src/inversion/inversion.js
@@ -1,4 +1,3 @@
-import { getXY } from '../core/core.js';
 import { dist, pointDifference, pointScale, pointSum } from '../point/point.js';
 import { circleStyle } from '../style/style.js';
 
@@ -12,3 +11,19 @@ import { circleStyle } from '../style/style.js';
 export function invertPoint(O, r, P) {
     return pointSum(O, pointScale(pointDifference(P, O), r ** 2 / dist(P, O) ** 2));
 }
+
+/**
+     * Given point O, radius r, and point P, radius s, returns the inverse of the circle centered at P with radius s
+     * with respect tot he circle centered at O with radius r
+     * @param O center
+     * @param r radius
+     * @param P center
+     * @param s radius
+     * @returns inverse of circle
+     */
+export function invertCircle(board, O, r, P, s, style = circleStyle) {
+    const center = () => pointSum(O, pointScale(pointDifference(P, O), r ** 2 / (dist(O, P) ** 2 - s ** 2)));
+    const radius = () => r ** 2 * s / (dist(O, P) ** 2 - s ** 2);
+    return board.create('circle', [center, radius], style);
+}
+
