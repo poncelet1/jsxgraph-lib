41db221b670081bd0b960f47da03993d4805839d
poncelet1 <nsato7@yahoo.ca>
2026-04-13 07:52:56 -0700

    Update core.js

--- src/core/core.js
+++ src/core/core.js
@@ -1,15 +1,14 @@
- /**
+/**
      * Given point P, returns x and y coordinates
      * @param P point
      * @returns [x, y]
      */
-export function getXY(P) {
-    return (typeof P.X === 'function')
-        ? [P.X(), P.Y()]
-        : P;
+export
+function getXY(P) {
+  return (typeof P.X === 'function') ? [P.X(), P.Y()] : P;
 }
 
- /**
+/**
      * Given a, b, c, returns the root of ax^2 + bx + c = 0
      * @param a coefficient of x^2
      * @param b coefficient of x
@@ -17,6 +16,7 @@ export function getXY(P) {
      * @param n 0 or 1
      * @returns root
      */
-export function quadraticRoot(a, b, c, n) {
-    return (-b + (2 * n - 1) * Math.sqrt(b ** 2 - 4 * a * c)) / (2 * a);
+export
+function quadraticRoot(a, b, c, n) {
+  return ( - b + (2 * n - 1) * Math.sqrt(b * *2 - 4 * a * c)) / (2 * a);
 }
