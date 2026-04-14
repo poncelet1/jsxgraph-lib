e405dbdfd1e7aef1f31fe536ae4fae6fbadad11a
poncelet1 <nsato7@yahoo.ca>
2026-04-13 18:47:24 -0700

    Update point.js

--- src/point/point.js
+++ src/point/point.js
@@ -1,173 +1,169 @@
-import { getXY } from '../core/core.js';
+import { getXY } from "../core/core.js";
 
 /**
-     * Given points A, B, returns the sum of A and B
-     * @param A first point
-     * @param B second dpoint
-     * @returns sum
-     */
+ * Given points A, B, returns the sum of A and B
+ * @param A first point
+ * @param B second dpoint
+ * @returns sum
+ */
 export function pointSum(A, B) {
-    const [ax, ay] = getXY(A);
-    const [bx, by] = getXY(B);
-    
-    return [
-        ax + bx,
-        ay + by
-    ];
+  const [ax, ay] = getXY(A);
+  const [bx, by] = getXY(B);
+
+  return [ax + bx, ay + by];
 }
 
 /**
-     * Given points A, B, returns the difference of A and B
-     * @param A first point
-     * @param B second dpoint
-     * @returns difference
-     */
+ * Given points A, B, returns the difference of A and B
+ * @param A first point
+ * @param B second dpoint
+ * @returns difference
+ */
 export function pointDifference(A, B) {
-    const [ax, ay] = getXY(A);
-    const [bx, by] = getXY(B);
-    
-    return [
-        ax - bx,
-        ay - by
-    ];
+  const [ax, ay] = getXY(A);
+  const [bx, by] = getXY(B);
+
+  return [ax - bx, ay - by];
 }
 
 /**
-     * Given point A and real number k, return kA
-     * @param A point
-     * @param k real number
-     * @returns kA
-     */
+ * Given point A and real number k, return kA
+ * @param A point
+ * @param k real number
+ * @returns kA
+ */
 export function pointScale(A, k) {
-    const [ax, ay] = getXY(A);
-    return [k * ax, k * ay];
+  const [ax, ay] = getXY(A);
+  return [k * ax, k * ay];
 }
 
 /**
-     * Given points A, B, returns the distance betweeen A and B
-     * @param A first point
-     * @param B second point
-     * @returns distance
-     */
+ * Given points A, B, returns the distance betweeen A and B
+ * @param A first point
+ * @param B second point
+ * @returns distance
+ */
 export function dist(A, B = [0, 0]) {
-    const [ax, ay] = getXY(A);
-    const [bx, by] = getXY(B);
-    return Math.hypot(bx - ax, by - ay);
+  const [ax, ay] = getXY(A);
+  const [bx, by] = getXY(B);
+  return Math.hypot(bx - ax, by - ay);
 }
 
 /**
-     * Given points A, B, returns the slope of line AB
-     * @param A first point
-     * @param B second point
-     * @returns slope
-     */
+ * Given points A, B, returns the slope of line AB
+ * @param A first point
+ * @param B second point
+ * @returns slope
+ */
 export function slope(A, B) {
-    const [ax, ay] = getXY(A);
-    const [bx, by] = getXY(B);
-    return (by - ay)/(bx - ax);
+  const [ax, ay] = getXY(A);
+  const [bx, by] = getXY(B);
+  return (by - ay) / (bx - ax);
 }
 
 /**
-     * Given points A, B, returns the y-intercept of line AB
-     * @param A first point
-     * @param B second point
-     * @returns y-intercept
-     */
+ * Given points A, B, returns the y-intercept of line AB
+ * @param A first point
+ * @param B second point
+ * @returns y-intercept
+ */
 export function yintercept(A, B) {
-    const [ax, ay] = getXY(A);
-    const [bx, by] = getXY(B);
-    return (ax * by - ay * bx)/(ax - bx);
+  const [ax, ay] = getXY(A);
+  const [bx, by] = getXY(B);
+  return (ax * by - ay * bx) / (ax - bx);
 }
 
 /**
-     * Given points A, B, C, D, returns the intersection of lines AB and CD
-     * @param A first point
-     * @param B second point
-     * @param C third point
-     * @param D fourth point
-     * @returns intersection
-     */
+ * Given points A, B, C, D, returns the intersection of lines AB and CD
+ * @param A first point
+ * @param B second point
+ * @param C third point
+ * @param D fourth point
+ * @returns intersection
+ */
 export function intersection(A, B, C, D) {
-    const [ax, ay] = getXY(A);
-    const [bx, by] = getXY(B);
-    const [cx, cy] = getXY(C);
-    const [dx, dy] = getXY(D);
-    return [
-        -((ax - bx)*(cx * dy - cy * dx) - (cx - dx)*(ax * by - ay * bx))/((ax - bx)*(cy - dy) - (ay - by)*(cx - dx)),
-        -((ay - by)*(cy * dx - cx * dy) - (cy - dy)*(ay * bx - ax * by))/((ay - by)*(cx - dx) - (ax - bx)*(cy - dy))
-        ];
-        
+  const [ax, ay] = getXY(A);
+  const [bx, by] = getXY(B);
+  const [cx, cy] = getXY(C);
+  const [dx, dy] = getXY(D);
+  return [
+    -((ax - bx) * (cx * dy - cy * dx) - (cx - dx) * (ax * by - ay * bx)) /
+      ((ax - bx) * (cy - dy) - (ay - by) * (cx - dx)),
+    -((ay - by) * (cy * dx - cx * dy) - (cy - dy) * (ay * bx - ax * by)) /
+      ((ay - by) * (cx - dx) - (ax - bx) * (cy - dy)),
+  ];
 }
 
- /**
-     * Given points A, B and real number t, returns the point P such that AP:AB = t
-     * @param A first point
-     * @param B second point
-     * @param t real number
-     * @returns interpolated point
-     */
+/**
+ * Given points A, B and real number t, returns the point P such that AP:AB = t
+ * @param A first point
+ * @param B second point
+ * @param t real number
+ * @returns interpolated point
+ */
 export function interp(A, B, t) {
-    const [ax, ay] = getXY(A);
-    const [bx, by] = getXY(B);
+  const [ax, ay] = getXY(A);
+  const [bx, by] = getXY(B);
 
-    return [
-        ax + t * (bx - ax),
-        ay + t * (by - ay)
-    ];
+  return [ax + t * (bx - ax), ay + t * (by - ay)];
 }
 
 /**
-     * Given points A, B, returns the midpoint of AB
-     * @param A first point
-     * @param B second point
-     * @returns midpoint
-     */
+ * Given points A, B, returns the midpoint of AB
+ * @param A first point
+ * @param B second point
+ * @returns midpoint
+ */
 export function midpoint(A, B) {
-    return interp(A, B, 1/2);
+  return interp(A, B, 1 / 2);
 }
 
 /**
-     * Given points A, B, C returns the projection of C onto line AB
-     * @param A first point
-     * @param B second point
-     * @param C second point
-     * @returns projection
-     */
+ * Given points A, B, C returns the projection of C onto line AB
+ * @param A first point
+ * @param B second point
+ * @param C second point
+ * @returns projection
+ */
 export function project(A, B, C) {
-    const [ax, ay] = getXY(A);
-    const [bx, by] = getXY(B);
-    const [cx, cy] = getXY(C);
+  const [ax, ay] = getXY(A);
+  const [bx, by] = getXY(B);
+  const [cx, cy] = getXY(C);
 
-    return [
-        ((cx - ax)*(bx - ax) + (cy - ay)*(by - ay))*(bx - ax)/dist(A,B) ** 2 + ax,
-        ((cx - ax)*(bx - ax) + (cy - ay)*(by - ay))*(by - ay)/dist(A,B) ** 2 + ay
-    ];
+  return [
+    (((cx - ax) * (bx - ax) + (cy - ay) * (by - ay)) * (bx - ax)) /
+      dist(A, B) ** 2 +
+      ax,
+    (((cx - ax) * (bx - ax) + (cy - ay) * (by - ay)) * (by - ay)) /
+      dist(A, B) ** 2 +
+      ay,
+  ];
 }
 
 /**
-     * Given points A, B, C returns the reflection of C over line AB
-     * @param A first point
-     * @param B second point
-     * @param C second point
-     * @returns reflection
-     */
+ * Given points A, B, C returns the reflection of C over line AB
+ * @param A first point
+ * @param B second point
+ * @param C second point
+ * @returns reflection
+ */
 export function reflect(A, B, C) {
-    return interp(C, project(A, B, C), 2);
+  return interp(C, project(A, B, C), 2);
 }
 
 /**
-     * Given point C, real number t, and point P, returns the rotation of P around C by an angle of t
-     * @param C center of rotation
-     * @param t angle of rotation
-     * @param P point being rotated
-     * @returns rotated point
-     */
+ * Given point C, real number t, and point P, returns the rotation of P around C by an angle of t
+ * @param C center of rotation
+ * @param t angle of rotation
+ * @param P point being rotated
+ * @returns rotated point
+ */
 export function rotate(C, t, P) {
-    const [cx, cy] = getXY(C);
-    const [px, py] = getXY(P);
+  const [cx, cy] = getXY(C);
+  const [px, py] = getXY(P);
 
-    return [
-        (px - cx) * Math.cos(t) - (py - cy) * Math.sin(t) + cx,
-        (px - cx) * Math.sin(t) + (py - cy) * Math.cos(t) + cy
-    ];
+  return [
+    (px - cx) * Math.cos(t) - (py - cy) * Math.sin(t) + cx,
+    (px - cx) * Math.sin(t) + (py - cy) * Math.cos(t) + cy,
+  ];
 }
