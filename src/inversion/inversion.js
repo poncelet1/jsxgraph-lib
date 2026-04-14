95e400eb54a8f97a31c8712e18df2c4b62deb3da
poncelet1 <nsato7@yahoo.ca>
2026-04-13 18:45:00 -0700

    Update inversion.js

--- src/inversion/inversion.js
+++ src/inversion/inversion.js
@@ -1,29 +1,35 @@
-import { dist, pointDifference, pointScale, pointSum } from '../point/point.js';
-import { circleStyle } from '../style/style.js';
+import { dist, pointDifference, pointScale, pointSum } from "../point/point.js";
+import { circleStyle } from "../style/style.js";
 
 /**
-     * Given point O, radius r, and point P, returns the inverse of P with respect to the circle centered at O with radius r.
-     * @param O center
-     * @param r radius
-     * @param P point
-     * @returns inverse of P
-     */
+ * Given point O, radius r, and point P, returns the inverse of P with respect to the circle centered at O with radius r.
+ * @param O center
+ * @param r radius
+ * @param P point
+ * @returns inverse of P
+ */
 export function invertPoint(O, r, P) {
-    return pointSum(O, pointScale(pointDifference(P, O), r ** 2 / dist(P, O) ** 2));
+  return pointSum(
+    O,
+    pointScale(pointDifference(P, O), r ** 2 / dist(P, O) ** 2),
+  );
 }
 
 /**
-     * Given point O, radius r, and point P, radius s, returns the inverse of the circle centered at P with radius s
-     * with respect tot he circle centered at O with radius r
-     * @param O center
-     * @param r radius
-     * @param P center
-     * @param s radius
-     * @returns inverse of circle
-     */
+ * Given point O, radius r, and point P, radius s, returns the inverse of the circle centered at P with radius s
+ * with respect to the circle centered at O with radius r
+ * @param O center
+ * @param r radius
+ * @param P center
+ * @param s radius
+ * @returns inverse of circle
+ */
 export function invertCircle(board, O, r, P, s, style = circleStyle) {
-    const center = () => pointSum(O, pointScale(pointDifference(P, O), r ** 2 / (dist(O, P) ** 2 - s ** 2)));
-    const radius = () => r ** 2 * s / (dist(O, P) ** 2 - s ** 2);
-    return board.create('circle', [center, radius], style);
+  const center = () =>
+    pointSum(
+      O,
+      pointScale(pointDifference(P, O), r ** 2 / (dist(O, P) ** 2 - s ** 2)),
+    );
+  const radius = () => (r ** 2 * s) / (dist(O, P) ** 2 - s ** 2);
+  return board.create("circle", [center, radius], style);
 }
-
