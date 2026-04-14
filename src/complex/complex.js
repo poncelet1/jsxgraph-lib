63f7046f58d8b588fe426332b42e32fb0e46acba
poncelet1 <nsato7@yahoo.ca>
2026-04-13 18:27:02 -0700

    Update complex.js

--- src/complex/complex.js
+++ src/complex/complex.js
@@ -1,76 +1,67 @@
-import { getXY } from '../core/core.js';
+import { getXY } from "../core/core.js";
 
 /**
-     * Given point A, returns the complex conjugate of A
-     * @param A point
-     * @returns conjugate
-     */
+ * Given point A, returns the complex conjugate of A
+ * @param A point
+ * @returns conjugate
+ */
 export function complexConjugate(A) {
-    const [ax, ay] = getXY(A);
-    
-    return [ax, -ay];
+  const [ax, ay] = getXY(A);
+
+  return [ax, -ay];
 }
 
 /**
-     * Given points A, B, returns the complex sum of A and B
-     * @param A first point
-     * @param B second dpoint
-     * @returns sum
-     */
+ * Given points A, B, returns the complex sum of A and B
+ * @param A first point
+ * @param B second dpoint
+ * @returns sum
+ */
 export function complexSum(A, B) {
-    const [ax, ay] = getXY(A);
-    const [bx, by] = getXY(B);
-    
-    return [
-        ax + bx,
-        ay + by
-    ];
+  const [ax, ay] = getXY(A);
+  const [bx, by] = getXY(B);
+
+  return [ax + bx, ay + by];
 }
 
 /**
-     * Given points A, B, returns the complex difference of A and B
-     * @param A first point
-     * @param B second dpoint
-     * @returns difference
-     */
+ * Given points A, B, returns the complex difference of A and B
+ * @param A first point
+ * @param B second dpoint
+ * @returns difference
+ */
 export function complexDifference(A, B) {
-    const [ax, ay] = getXY(A);
-    const [bx, by] = getXY(B);
-    
-    return [
-        ax - bx,
-        ay - by
-    ];
+  const [ax, ay] = getXY(A);
+  const [bx, by] = getXY(B);
+
+  return [ax - bx, ay - by];
 }
 
 /**
-     * Given points A, B, returns the complex product of A and B
-     * @param A first point
-     * @param B second dpoint
-     * @returns product
-     */
+ * Given points A, B, returns the complex product of A and B
+ * @param A first point
+ * @param B second dpoint
+ * @returns product
+ */
 export function complexProduct(A, B) {
-    const [ax, ay] = getXY(A);
-    const [bx, by] = getXY(B);
-    
-    return [
-        ax * bx - ay * by,
-        ax * by + ay * bx
-    ];
+  const [ax, ay] = getXY(A);
+  const [bx, by] = getXY(B);
+
+  return [ax * bx - ay * by, ax * by + ay * bx];
 }
 
 /**
-     * Given points A, B, returns the complex quotient of A and B
-     * @param A first point
-     * @param B second dpoint
-     * @returns quotient
-     */
+ * Given points A, B, returns the complex quotient of A and B
+ * @param A first point
+ * @param B second dpoint
+ * @returns quotient
+ */
 export function complexQuotient(A, B) {
-    const [ax, ay] = getXY(A);
-    const [bx, by] = getXY(B);
-    
-    return [
-        (ax * bx + ay * by)/(bx ** 2 + by ** 2),
-        (ay * bx - ax * by)/(bx ** 2 + by ** 2)
-    ];
+  const [ax, ay] = getXY(A);
+  const [bx, by] = getXY(B);
+
+  return [
+    (ax * bx + ay * by) / (bx ** 2 + by ** 2),
+    (ay * bx - ax * by) / (bx ** 2 + by ** 2),
+  ];
 }
