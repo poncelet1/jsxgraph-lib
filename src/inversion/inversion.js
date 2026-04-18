d37348b348275c5555f89f4c359151b90c90be5e
poncelet1 <nsato7@yahoo.ca>
2026-04-17 19:30:08 -0700

    Update inversion.js

--- src/inversion/inversion.js
+++ src/inversion/inversion.js
@@ -29,17 +29,17 @@ export function invertPoint(circ, P) {
 export function invertCircle(board, circ1, circ2, style = circleStyle) {
   const o1x = () => circ1.center.X();
   const o1y = () => circ1.center.Y();
-  const r1 = circ1.Radius();
+  const r1 () => circ1.Radius();
   const o2x = () => circ2.center.X();
   const o2y = () => circ2.center.Y();
-  const r2 = circ2.Radius();
+  const r2 () => circ2.Radius();
 
-  const denom = () => Math.hypot(o1x() - o2x(), o1y() - o2y()) ** 2 - r2 ** 2;
+  const denom = () => Math.hypot(o1x() - o2x(), o1y() - o2y()) ** 2 - r2() ** 2;
   
   const center = () => [
-    r1 ** 2 / denom() * (o2x() - o1x()) + o1x(),
-    r1 ** 2 / denom() * (o2y() - o1y()) + o1y()
+    r1() ** 2 / denom() * (o2x() - o1x()) + o1x(),
+    r1() ** 2 / denom() * (o2y() - o1y()) + o1y()
   ];
-  const radius = () => (r1 ** 2 * r2) / denom();
+  const radius = () => (r1() ** 2 * r2()) / denom();
   return board.create("circle", [center, radius], style);
 }
